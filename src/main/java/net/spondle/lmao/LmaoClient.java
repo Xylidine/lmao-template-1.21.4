81df1dd1648b3ed12028832c19b3137566ca37de
Xylidine <spondle528@gmail.com>
2024-12-23 08:21:25 -0500

    Idk what to put here

---
+++ src/main/java/net/spondle/lmao/LmaoClient.java
@@ -0,0 +1,11 @@
+package net.spondle.lmao;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LmaoClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/spondle/lmao/LmaoDataGenerator.java
@@ -0,0 +1,12 @@
+package net.spondle.lmao;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LmaoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+
+	}
+}
